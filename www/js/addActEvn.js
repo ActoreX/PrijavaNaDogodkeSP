d3da924ec340abb7b4153d67056d866241539e0d
Aleksander Tomic <ALEX@Aleksanders-MacBook-Pro.local>
2015-11-21 14:01:36 +0100

    Pred spremembami

--- www/js/addActEvn.js
+++ www/js/addActEvn.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-    $("input[name=omejitev-spinner]").spinner();
-    $("input[name=omejitev-spinner]").spinner().val("100");
-    $("input[name=omejitev-spinner]").spinner("option", "min", 0);
-    $("input[name=omejitev-spinner]").spinner("option", "max", 500);
+    var omejitevStMest = $("input[name=omejitev-spinner]").spinner();
+    omejitevStMest.val("100");
+    omejitevStMest.spinner("option", "min", 0);
+    omejitevStMest.spinner("option", "max", 500);
 
-    $("input[name=datum-od]").datepicker();
-    $("input[name=datum-do]").datepicker();
+    var datumOd = $("input[name=datum-od]").datepicker();
+    var datumDo = $("input[name=datum-do]").datepicker();
+
 
     mojaLokacija = new Object();
     mojaLokacija.latitude = 46.057353;
@@ -35,5 +36,85 @@ $(document).ready(function(){
 
 
     $('#omejitev-spinner').spinner();
+    
+    var jeEnkratna = $('input[name=vrstaIzbireTrajanja]:checked').val();
+    nastaviPoljaZaUstreznoIzbiroTrajanja(jeEnkratna);
+    
+    var jeBrezplacna = $('select[name=tipAktEvn] option:selected').val();
+    nastaviPoljaZaUstreznoIzbiroTipa(jeBrezplacna);
+    
+    var jeOmejenoStMest = $('select[name=stMestOmejitevAktEvn] option:selected').val();
+    nastaviPoljaZaUstreznoIzbiroOmejitevStMest(jeOmejenoStMest);
+    
+    $('input[name=vrstaIzbireTrajanja]').change(function(param){
+        //console.log(param.currentTarget.value); 
+        nastaviPoljaZaUstreznoIzbiroTrajanja(param.currentTarget.value)
+            
+    });
+    $('select[name=tipAktEvn]').change(function(param){
+        console.log(param.currentTarget.value); 
+        nastaviPoljaZaUstreznoIzbiroTipa(param.currentTarget.value)
+            
+    });  
+    
+    $('select[name=stMestOmejitevAktEvn]').change(function(param){
+        console.log(param.currentTarget.value); 
+        nastaviPoljaZaUstreznoIzbiroOmejitevStMest(param.currentTarget.value)
+            
+    });
+    
+    
+    
+    function nastaviPoljaZaUstreznoIzbiroTrajanja(trajanje) {
+        var datumDo = $('input[name=datum-do]');
+        if(trajanje == "enkratna") {
+            datumDo.val("");
+            datumDo.prop("disabled", false);
+        } else if(trajanje == "ponavljajoca") {
+            datumDo.val("/");
+            datumDo.prop("disabled", true);
+        } else {
+            alert("nastaviPoljaZaUsteznoIzbiroTrajanja parameter ni veljaven!");
+        }
+    }
+    
+    function nastaviPoljaZaUstreznoIzbiroTipa(tip) {
+        var cenaVstopnice = $('input[name=cenaVstopnice]');
+        if(tip == "brezplacna") {
+            cenaVstopnice.val("0");
+            cenaVstopnice.prop("disabled", true);
+            
+        } else if (tip == "placljiva") {
+            cenaVstopnice.prop("disabled", false);
+        } else {
+            alert("nastaviPoljaZaUstreznoIzbiroTipa paramtere ni veljaven!");
+        }
+    }
+    
+    function nastaviPoljaZaUstreznoIzbiroOmejitevStMest(omejitev) {
+        var steviloMestSpinner = $("input[name=omejitev-spinner]").spinner();
 
+        if(omejitev == "neomejeno") {
+            steviloMestSpinner.val("inf");  
+            steviloMestSpinner.spinner("disable");
+            
+        } else if (omejitev == "omejeno") {
+            steviloMestSpinner.val(100);
+            steviloMestSpinner.spinner("enable");
+        } else {
+            alert("nastaviPoljaZaUstreznoIzbiroTipa paramtere ni veljaven!");
+        }
+    }
+    
+    $('.novaAktEvn').validate({
+       rules: {
+           nazivIzbire: "required"
+       },
+        messages: {
+            nazivIzbire: "Bla"
+        }
+        
+    });
+    
 });
+
