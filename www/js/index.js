98db4e65dbb734f3e4b8aa97ff97f5455f486fa2
Aleksander Tomic <ALEX@Aleksanders-MacBook-Pro.local>
2015-11-22 16:03:50 +0100

    Popravi domače strani, velikosti gumbov

--- www/js/index.js
+++ www/js/index.js
@@ -20,9 +20,11 @@ $(document).ready(function(){
         if($(this).hasClass("env-follow") == true){
             $(this).addClass("env-unfollow").removeClass("env-follow");
             $(this).addClass("btn-warning").removeClass("btn-info");    
+            $(this).val("Ne sledi");
         } else {    
             $(this).addClass("env-follow").removeClass("env-unfollow")
             $(this).addClass("btn-info").removeClass("btn-warning");
+            $(this).val("Sledi");
         }
         
     });   
@@ -31,10 +33,12 @@ $(document).ready(function(){
     $('.env-attend, .env-cancel').click(function(){
         if($(this).hasClass("env-attend") == true){
             $(this).addClass("env-cancel").removeClass("env-attend");
-            $(this).addClass("btn-danger").removeClass("btn-success");    
+            $(this).addClass("btn-danger").removeClass("btn-success"); 
+            $(this).val("Prekliči udeležbo");
         } else {    
             $(this).addClass("env-attend").removeClass("env-cancel")
             $(this).addClass("btn-success").removeClass("btn-danger");
+          $(this).val("Potrdi udeležbo");
         }
         
     }); 
