1d6a3870842ad5205880be47aa5482f907d15a0c
Aleksander Tomic <actorex@gmail.com>
2015-12-27 16:43:14 +0100

    Dodana prva za silo delujoča verzija dodajanja nove aktivnosti - potrebno popraviti format date time pri datetimepicker

--- EventiqonWebApp/EventiqonWebApp/Controllers/OrganizerController.cs
+++ EventiqonWebApp/EventiqonWebApp/Controllers/OrganizerController.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EventiqonWebApp.Models;
-using System.Net.Http;
 using RestSharp;
+using System.Transactions;
 
 namespace EventiqonWebApp.Controllers
 {
     public class OrganizerController : Controller
     {
+
+        EventiqonEntities db = new EventiqonEntities();
+
         // GET: Organizer/AddNewActivity
         [HttpGet]
         public ActionResult AddNewActivity()
@@ -22,24 +25,143 @@ namespace EventiqonWebApp.Controllers
         [HttpPost]
         public JsonResult AddNewActivity(AktivnostDogodekVhodniPodatki vhod)
         {
-            // GET: (klic Google Maps APIja za geodecoding
-            // https://maps.googleapis.com/maps/api/geocode/json?latlng=46.06813252000273,14.544028793945245&key=AIzaSyAmZRoA4MHaKQDY4JuWnkV3ZB-k69kluts
-        
-            var restClient = new RestClient("https://maps.googleapis.com");
-            var request = new RestRequest(Method.GET);
-            request.Resource = "maps/api/geocode/json";
-            request.AddParameter("latlng", "46.06813252000273,14.544028793945245");
-            request.AddParameter("key", "AIzaSyAmZRoA4MHaKQDY4JuWnkV3ZB-k69kluts");
-        
-            var response = restClient.Execute<GoogleMapsGeocodingModel>(request);
+            string odgovor = "Odgovor strežnka: ";
+            GoogleMapsGeocodingModel googleMapsModel = null;
+            if (obveznaPoljaPodanaAktivnostDogodekVhodniPodatki(vhod))
+            {
+                // GET: (klic Google Maps APIja za geodecoding
+                // https://maps.googleapis.com/maps/api/geocode/json?latlng=46.06813252000273,14.544028793945245&key=AIzaSyAmZRoA4MHaKQDY4JuWnkV3ZB-k69kluts
 
-            GoogleMapsGeocodingModel googleMapsModel = response.Data;
+                var restClient = new RestClient("https://maps.googleapis.com");
+                var request = new RestRequest(Method.GET);
+                request.Resource = "maps/api/geocode/json";
+                request.AddParameter("latlng", "46.06813252000273,14.544028793945245");
+                request.AddParameter("key", "AIzaSyAmZRoA4MHaKQDY4JuWnkV3ZB-k69kluts");
 
+                var response = restClient.Execute<GoogleMapsGeocodingModel>(request);
+
+                googleMapsModel = response.Data;
+                if(googleMapsModel.status == "OK")
+                {
+                    var kraj = googleMapsModel.results[5].address_components[1].long_name;
+                    var drzavaKratica = googleMapsModel.results[5].address_components[2].short_name;
+                    var drzavaIme = googleMapsModel.results[5].address_components[2].long_name;
+
+                    using (TransactionScope ts = new TransactionScope(TransactionScopeOption.RequiresNew))
+                    {
+                        try {
+                            Kraj kr = null;
+                            // preveri če mesto v bazi že obstaja 
+                            if (!db.Kraj.Any(k => k.nazivKraja == kraj))
+                            {
+                                kr = new Kraj();
+                                kr.nazivKraja = kraj;
+                                kr.idKraj = db.Kraj.Max(k => k.idKraj) + 1;
+                                db.Kraj.Add(kr);
+                                db.SaveChanges();
+
+                            }
+                            else
+                            {
+                                kr = db.Kraj.Where(k => k.nazivKraja == kraj).Take(1).Single();
+                            }
+
+                            Drzava dr = null;
+                            // Preveri ali država obstaja, če ne jo dodaj
+                            if (!db.Drzava.Any(d => d.kraticaDrzave == drzavaKratica))
+                            {
+                                dr = new Drzava();
+                                dr.kraticaDrzave = drzavaKratica;
+                                dr.imeDrzave = drzavaIme;
+                                db.Drzava.Add(dr);
+                                db.SaveChanges();
+                                odgovor += "Država vstavljena v db! ";
+                            }
+                            else
+                            {
+                                dr = db.Drzava.Where(d => d.kraticaDrzave == drzavaKratica).Take(1).Single();
+                            }
+
+                            Aktivnost novaAktivnost = new Aktivnost();
+                            novaAktivnost.idAktivnost = db.Aktivnost.Max(a => a.idAktivnost) + 1;
+                            novaAktivnost.idKraj = kr.idKraj;
+                            novaAktivnost.jeEnkratna = (vhod.vrstaIzbireTrajanja == "enkratna");
+                            novaAktivnost.kraticaDrzave = dr.kraticaDrzave;
+                            novaAktivnost.latitude = Convert.ToDouble(vhod.geolocation_lat);
+                            novaAktivnost.longitude = Convert.ToDouble(vhod.geolocation_lon);
+                            novaAktivnost.naziv = vhod.nazivIzbire; // naziv aktivonosti (dogodka)
+                            novaAktivnost.nazivLokacije = vhod.vnosLokacije;
+                            novaAktivnost.opis = vhod.opisAktEvn;
+                            novaAktivnost.status = "neznano";
+                            novaAktivnost.cena = vhod.cenaVstopnice;
+                            novaAktivnost.datumOd = Convert.ToDateTime(vhod.datum_od);
+                            // če datum do obstaja
+                            if (vhod.datum_do != null)
+                            {
+                                novaAktivnost.datumDo = Convert.ToDateTime(vhod.datum_do);
+                            }
+                            if (vhod.stMestOmejitevAktEvn != null && vhod.stMestOmejitevAktEvn == "omejeno")
+                            {
+                                novaAktivnost.omejitevMest = Convert.ToInt32(vhod.steviloMestSpinner);
+                            }
+                            novaAktivnost.datumVnosa = DateTime.Now;
+
+                            db.Aktivnost.Add(novaAktivnost);
+                            db.SaveChanges();
+
+                            // postavke aktivnosti(dogodka)
+                            foreach (var postavka in vhod.seznamPostavk)
+                            {
+                                PostavkaAktivnosti novaPostavka = new PostavkaAktivnosti();
+                                novaPostavka.idPostavkaAktivnosti = db.PostavkaAktivnosti.Max(pa => pa.idPostavkaAktivnosti) + 1;
+                                novaPostavka.idAktivnost = novaAktivnost.idAktivnost;
+                                novaPostavka.casovniOpisKdaj = postavka.casPostavke;
+                                novaPostavka.opisCasaTrajanja = postavka.casTrajanjaPostavke;
+                                novaPostavka.naziv = postavka.nazivPostavke;
+                                novaPostavka.polniOpis = postavka.opisPostavke;
+                                db.PostavkaAktivnosti.Add(novaPostavka);
+                                db.SaveChanges();
+                            }
+
+
+
+                            // commitaj transakcijo
+
+                            ts.Complete();
+
+                        } catch (Exception ex)
+                        {
+                            var nekijenarobe = ex;
+                        }
+                    }
+
+                } else
+                {
+                    odgovor += "Napaka pri pridobivanju podatkov o geolokaciji";
+                }
+               
+
+            } else
+            {
+                odgovor += "Neustrezen vnos forme !";
+            }
             return Json("Super");
         }
 
+        bool obveznaPoljaPodanaAktivnostDogodekVhodniPodatki(AktivnostDogodekVhodniPodatki vhod)
+        {
+            // preveri obvezna polja
+            if (vhod.nazivIzbire != null && vhod.datum_od != null && vhod.vrstaIzbireTrajanja != null && vhod.geolocation_lat != null && vhod.geolocation_lon != null && vhod.vnosLokacije != null
+                && vhod.opisAktEvn != null && (vhod.vrstaIzbireTrajanja == "enkratna" && vhod.datum_do != null || vhod.vrstaIzbireTrajanja == "ponavljajoca"))
+                return true;
+
+            return false;
+        }
+
+      
+
         // GET: Organizer/AddNewEvent
-        [HttpGet]
+        [HttpGet]   
         public ActionResult AddNewEvent()
         {
             return View();
--- EventiqonWebApp/EventiqonWebApp/Models/Drzava.cs
+++ EventiqonWebApp/EventiqonWebApp/Models/Drzava.cs
@@ -24,6 +24,7 @@ namespace EventiqonWebApp.Models
     
         public string kraticaDrzave { get; set; }
         public string imeDrzave { get; set; }
+
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Aktivnost> Aktivnost { get; set; }
