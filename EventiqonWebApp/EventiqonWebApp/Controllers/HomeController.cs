48e2a348b68059218e0ea8f2ea3876ad5e6d2e85
Aleksander Tomic <actorex@gmail.com>
2016-01-14 18:31:35 +0100

    Branje akktivnosti in dogodkov iz PB

--- EventiqonWebApp/EventiqonWebApp/Controllers/HomeController.cs
+++ EventiqonWebApp/EventiqonWebApp/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using EventiqonWebApp.Models;
 namespace EventiqonWebApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        EventiqonEntities db = new EventiqonEntities();
+
+
         public ActionResult Index()
         {
-            return View();
+            var zadnjiDogodki = db.Dogodek.OrderByDescending(d => d.datumVnosa).Take(10);
+            return View(zadnjiDogodki);
         }
 
     }
